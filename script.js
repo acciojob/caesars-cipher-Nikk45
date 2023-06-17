36c0f7815e093df0738007cad0226342b2b60dd7
Nikk45 <72194228+Nikk45@users.noreply.github.com>
2023-06-17 18:27:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ function rot13(str) {
 			decodedArr.push(lookup[str[i]]);
 		}
 	}
-  return decodedArr.join(""); //return decodedArr
+  return decodedArr; //return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
@@ -48,4 +48,4 @@ function rot13(str) {
  console.log(rot13("SERR YBIR? NPPVBWBO"));
 
 // Do not change this line
-window.rot13 = rot13;
+window.exports = rot13;
